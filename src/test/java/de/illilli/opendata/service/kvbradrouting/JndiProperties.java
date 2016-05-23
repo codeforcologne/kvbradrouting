19760d9eda902909491014534b616a60077f0e21
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-05-23 05:18:11 +0200

    change to kvbrad

--- src/test/java/de/illilli/opendata/service/kvbradrouting/JndiProperties.java
+++ src/test/java/de/illilli/opendata/service/kvbradrouting/JndiProperties.java
@@ -35,15 +35,13 @@ public class JndiProperties {
 
 	public static void setUpConnectionForJndi() throws IOException {
 
-		InputStream inputStream = JndiProperties.class
-				.getResourceAsStream(JNDI_PROPERTIES);
+		InputStream inputStream = JndiProperties.class.getResourceAsStream(JNDI_PROPERTIES);
 		Properties properties = new Properties();
 		properties.load(inputStream);
 
 		try {
 			// Create initial context
-			System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
-					"org.apache.naming.java.javaURLContextFactory");
+			System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
 			System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
 
 			InitialContext ic = new InitialContext();
@@ -55,13 +53,12 @@ public class JndiProperties {
 			// Construct DataSource
 			PGSimpleDataSource ds = new PGSimpleDataSource();
 			ds.setServerName(properties.getProperty("servername"));
-			ds.setPortNumber(Integer.parseInt(properties
-					.getProperty("portnumber")));
+			ds.setPortNumber(Integer.parseInt(properties.getProperty("portnumber")));
 			ds.setDatabaseName(properties.getProperty("databasename"));
 			ds.setUser(properties.getProperty("user"));
 			ds.setPassword(properties.getProperty("password"));
 
-			ic.bind("java:/comp/env/jdbc/kvbraeder", ds);
+			ic.bind("java:/comp/env/jdbc/kvbrad", ds);
 
 		} catch (NamingException ex) {
 			ex.printStackTrace();
